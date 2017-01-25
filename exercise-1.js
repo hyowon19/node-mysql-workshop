72659b1810cf05adff34671190b7f9a463f36820
Victor Wong <vj1901@hotmail.com>
2017-01-25 23:27:40 +0000

    exercise 1 updated

--- exercise-1.js
+++ exercise-1.js
@@ -1,23 +1,57 @@
 var mysql = require('mysql');
 var colors = require('colors');
-var mysqlPromise = require('mysql-promise');
+var mysqlPromise = require('mysql-promise')();
+// const Colors = require('colors-promise');
 
-var connection = mysql.createConnection({
-  host     : process.env.IP,
-  user     : process.env.C9_USER,
-  password : '',
-  database : 'addressbook'
+// CALLBACK VERSION - uncomment code below to use it, comment it out to not use it
+// var connection = mysql.createConnection({
+//   host     : process.env.IP,
+//   user     : process.env.C9_USER,
+//   password : '',
+//   database : 'addressbook'
+// });
+
+// function showDataBase() {
+//     connection.query("SHOW DATABASES", function(err, result) {
+//         if(err) {
+//             console.log(err);
+//         }
+//         else
+//         {
+//             console.log("Database".toUpperCase());
+//             result.forEach(function(results, i) {
+//                 console.log(results.Database.blue.bold);
+//             });
+//         }
+//         connection.end();
+//     });
+// }
+// showDataBase();
+// END OF CALLBACK VERSION
+
+
+//PROMISE VERSION - uncomment code below to use it, comment it out to not use it
+mysqlPromise.configure({
+	"host": process.env.IP,
+	"user": process.env.C9_USER,
+	"password": '',
+	"database": "addressbook"
 });
 
+function showDataBase() {
+    return mysqlPromise.query("SHOW DATABASES");
+}
 
-connection.query("SHOW DATABASES", function(err, result) {
-    if(err) {
-        console.log(err);
-    }
-    else{
-        result.forEach(function(results, i) {
-            console.log("Database".toUpperCase() + " " + results.Database.blue.bold);
-        });
-    }
-    connection.end();
-});
+showDataBase()
+.then(function(result){
+    console.log("Database".toUpperCase());
+    result[0].forEach(function(results,i){
+        console.log((results.Database).bold.blue);
+    });
+    mysqlPromise.end();
+})
+.catch(function(err){
+    console.log(err);
+    mysqlPromise.end();
+})
+// END OF PROMISE VERSION
