6af5e8a32662c4bca6b7d6acba38ce0c0b5a726f
Victor Wong <vj1901@hotmail.com>
2017-01-27 16:15:58 +0000

    exercise 4

---
+++ exercise-4.js
@@ -0,0 +1,67 @@
+var mysql = require('mysql');
+var color = require('colors');
+
+var connection = mysql.createConnection({
+  host     : process.env.IP,
+  user     : process.env.C9_USER,
+  password : '',
+  database : 'addressbook'
+});
+
+
+var query = `
+SELECT
+  Account.id AS accountId,
+  Account.email AS accountEmail,
+  AddressBook.id AS addressbookId,
+  AddressBook.name AS addressbookName
+FROM Account
+LEFT JOIN AddressBook on Account.id = AddressBook.accountId`;
+
+connection.query(query, function(err, result){
+  if(err) {
+    console.log('There is an error!');
+  }
+  else {
+    var accounts = [];
+    result.forEach(function(row) {
+      var account = accounts.find(function(account) {
+        return account.accountId === row.accountId;
+      });
+      
+      if(!account) {
+        account = {
+          accountId: row.accountId,
+          accountEmail: row.accountEmail,
+          addressbooks: []
+        };
+        accounts.push(account);
+      }
+      
+      var addressBook = account.addressbooks.find(function(addressbook) {
+        return addressbook.addressbookId === row.addressbookId;
+      });
+      
+      if(!addressBook) {
+        addressBook = {
+          name: row.addressbookName
+        };
+        account.addressbooks.push(addressBook);
+      }
+      
+    });
+    accounts.forEach(function(account){
+      console.log(('#' + account.accountId + ' ').bold.green + account.accountEmail.bold.blue);
+      for(var i = 0; i < account.addressbooks.length; i++) {
+        if (!account.addressbooks[i].name) {
+          console.log('   --no address books--');
+        }
+        else {
+          console.log('   (' + (i+1) + ') ' + account.addressbooks[i].name);
+        }
+        
+      }
+    });
+  }
+  connection.end();
+});
